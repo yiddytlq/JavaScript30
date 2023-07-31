1dde740dccdac0250c1a31c05bc83b1917588fc2
yiddy (VSCode JLM Office) <yidkon@gmail.com>
2023-07-31 21:57:03 +0300

    completed clock

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,23 @@
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 60) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    console.log(hours,minutes,seconds);
+}
+
+setInterval(setDate, 1000);
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
